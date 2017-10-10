84fd049735849ecc2c6fce4b18810fd53a6d73cf
e.zibzibadze <e.zibzibadze@EZEKI-PC>
2017-10-10 14:18:56 +0400

    updates

--- src/compose-reducers.ts
+++
@@ -1,5 +0,0 @@
-export function composeReducers(reducers: [any]) {
-	return (state, action) =>
-		reducers.reduce((newState, reducer) =>
-			reducer(newState, action), state);
-}
--- src/dynamic-middleware.ts
+++ src/dynamic-middleware.ts
@@ -1,52 +1,52 @@
-import * as _redux from 'redux';
+import { compose } from 'redux';
 
 function _toConsumableArray(arr) {
-    if (Array.isArray(arr)) {
-        const arr2 = Array(arr.length);
+	if (Array.isArray(arr)) {
+		const arr2 = Array(arr.length);
 
-        for (let i = 0; i < arr.length; i++) {
-            arr2[i] = arr[i];
-        }
+		for (let i = 0; i < arr.length; i++) {
+			arr2[i] = arr[i];
+		}
 
-        return arr2;
-    } else {
-        return Array.from(arr);
-    }
+		return arr2;
+	} else {
+		return Array.from(arr);
+	}
 }
 
 let dynamic = [];
 
 export function dynamicMiddlewares(store) {
-    return function (next) {
-        return function (action) {
-            const middlewareAPI = {
-                getState: store.getState,
-                dispatch: function dispatch(act) {
-                    return store.dispatch(act);
-                }
-            };
-            const chain = dynamic.map(function (middleware) {
-                return middleware(middlewareAPI);
-            });
-            return _redux.compose.apply(undefined, _toConsumableArray(chain))(next)(action);
-        };
-    };
+	return function (next) {
+		return function (action) {
+			const middlewareAPI = {
+				getState: store.getState,
+				dispatch: function dispatch(act) {
+					return store.dispatch(act);
+				}
+			};
+			const chain = dynamic.map(function (middleware) {
+				return middleware(middlewareAPI);
+			});
+			return compose.apply(undefined, _toConsumableArray(chain))(next)(action);
+		};
+	};
 }
 
 export function addMiddleware(middleware) {
-    let _dynamic;
-    dynamic = (_dynamic = dynamic).concat.apply(_dynamic, arguments);
+	let _dynamic;
+	dynamic = (_dynamic = dynamic).concat.apply(_dynamic, arguments);
 }
 
 export function removeMiddleware(middleware) {
-    const index = dynamic.findIndex(function (d) {
-        return d === middleware;
-    });
-    if (index === -1) { return; }
+	const index = dynamic.findIndex(function (d) {
+		return d === middleware;
+	});
+	if (index === -1) { return; }
 
-    dynamic = [].concat(_toConsumableArray(dynamic.slice(0, index)), _toConsumableArray(dynamic.slice(index + 1)));
+	dynamic = [].concat(_toConsumableArray(dynamic.slice(0, index)), _toConsumableArray(dynamic.slice(index + 1)));
 }
 
 export function resetMiddlewares() {
-    dynamic = [];
+	dynamic = [];
 }
